0674d9958d8b034bdabd3a9af8e658092e7043b2
leihaohao <2468048176@qq.com>
2025-07-06 20:55:18 +0800

    style(eslint): 修复代码风格

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { antfu } from '@antfu/eslint-config'
 
 export default antfu(
   {
-    ignores: [".github"],
+    ignores: ['.github'],
     type: 'lib',
     formatters: true,
     lessOpinionated: true,
